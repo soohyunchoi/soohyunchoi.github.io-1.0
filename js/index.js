26fa5257b0f3450315827f3027871cee1e8cc35f
officialsushi <soohyun9090@gmail.com>
2017-02-16 23:28:37 -0800

    beginnings
    humble

---
+++ js/index.js
@@ -0,0 +1,12 @@
+  $(document).ready(function() {
+    $("#main-subtext").css("margin", "-0.15em 0em 2em 0em");
+    $("#main-subtext").css("font-weight", "normal");
+		$("#fr-portrait")
+			.css("margin", "0 auto")
+			.addClass("portraitimg")
+			.css("display", "block")
+		$(".card-block").css("padding", "2em 0em 1em 0em");
+		$("#portrait-caption").addClass("card-text text-center main-caption");
+		$("#disco-main").css("padding", "0em 0em .5em 0em");
+		$('[data-toggle="tooltip"]').tooltip();
+  });
