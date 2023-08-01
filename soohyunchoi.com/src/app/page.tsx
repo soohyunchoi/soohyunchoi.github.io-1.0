f6b429f6468462ccd3a36486da77eba260b18624
Soohyun Choi <soohyun9090@gmail.com>
2023-07-31 21:44:29 -0700

    Finalize Figma design, lay groundwork

---
+++ soohyunchoi.com/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center justify-center text-black">
+            Hey! I'm Soohyun.
+        </div>
+    </main>
+  )
+}
